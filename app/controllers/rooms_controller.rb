83ab36300bb847cd52b2bf59e87debf0822b8d9e
username <username@example.com>
2022-08-10 01:15:38 +0900

    room投稿機能追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,27 @@
 class RoomsController < ApplicationController
   def new
+    @room = Room.new
+  end
+
+  def create
+    room = Room.new(room_params)
+    room.user_id = current_user.id
+    if room.save
+      #flash[:notice] = ""
+      redirect_to rooms_path
+    else
+      render "new"
+    end
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, :introduction, :price, :address, :image, :image_cache)
   end
 end
