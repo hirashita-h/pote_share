f102d5909e376f06d91e5f9926f52179720ad45e
username <username@example.com>
2022-08-10 00:14:18 +0900

    create 'rooms_controller'

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,10 @@
+class RoomsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :rooms, only: [:index, :show, :new, :create]
   devise_for :users
-  resources :users
+  resources :users, only: [:show, :edit, :update]
+  get 'users/account'
   root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
