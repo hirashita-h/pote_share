c94e275ab511e2b15f98f9660e53f06edb1f9cc6
username <username@example.com>
2022-08-10 20:43:17 +0900

    create 'confirm.html.erb'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :reservations, only: [:index, :new, :create]
+  resources :reservations, only: [:index, :new, :create] do
+    post :confirm, on: :member
+  end
   resources :rooms, only: [:index, :show, :new, :create]
   devise_for :users
   resources :users, only: [:show, :edit, :update] do
