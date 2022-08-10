4a3b001e82cc06cc504438251264ffd76c6fa4de
username <username@example.com>
2022-08-10 19:06:23 +0900

    modify 'room.rb'

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,7 @@ class Room < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :reservations
   has_many :users, through: :reservations
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
 end
