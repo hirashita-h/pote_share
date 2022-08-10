fceabdf516cb826f5d04e163c6bc9b03a063f002
username <username@example.com>
2022-08-10 20:44:40 +0900

    reservationのバリデーション設定

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  validates :number_of_people, presence: true
+  validates :check_in_day, presence: true
+  validates :check_out_day, presence: true
 end
