b2ae21493abd3cd5d5335b159a1f78b2442cc34d
username <username@example.com>
2022-08-13 13:40:36 +0900

    予約機能修正

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,13 +9,18 @@ class ReservationsController < ApplicationController
     # binding.pry
     @reservation = Reservation.new(reservation_params)
     session[:reservation] = @reservation
+    @stay_days = ((@reservation.check_out_day - @reservation.check_in_day) / 24 / 60 / 60).to_i
+    @number_of_people = @reservation.number_of_people
+    @room = Room.find(@reservation.room_id)
+    @price = @room.price
+    @total_price = @stay_days * @number_of_people * @price
     # render :new if @reservaion.invalid?
   end
 
   def create
     @reservation = Reservation.new(session[:reservation])
     if @reservation.save
-      redirect_to rooms_path, notice: "予約が完了しました"
+      redirect_to room_path(@reservation.room_id), notice: "予約が完了しました"
     else
       render "confirm"
     end
