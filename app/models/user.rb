ff1a56839c010f6f15d8b2c4d88bbcf5ff26581b
username <username@example.com>
2022-08-14 18:40:14 +0900

    viewファイル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :rooms, through: :reservations
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
 end
